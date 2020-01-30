98b380485c15878b667404bc199f52830ef8516b
Furkan <furkankocyigit58@gmail.com>
2020-01-29 22:00:55 -0800

    particle filter initial codes are added

--- KalmanFilter/test.py
+++ KalmanFilter/test.py
@@ -2,3 +2,11 @@
 value= [[0,1,2],[3,4,5],[6,7,8]]
 value = [[0 for row in range(len(value))] for col in range(len(value[0]))]
 print(value)
+
+a= [[1,2,3],
+    [1,2,3]]
+b= [[1,2,3],
+    [1,2,3]]
+
+c = a+b
+print("c:",c)
